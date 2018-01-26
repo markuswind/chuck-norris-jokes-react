a65849d0ea98dcda0f569a5ddec23078c3d30629
Markus Wind <markuswind@gmail.com>
2018-01-26 11:01:43 +0100

    moved registerserviceworker to config folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+
 import App from './containers/app/App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './config/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
