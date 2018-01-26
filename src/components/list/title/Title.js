42e2a636a817e3ed962bd3b01f84d80fc8c5fb4b
Markus Wind <markuswind@gmail.com>
2018-01-26 12:12:09 +0100

    added proptype to title component

--- src/components/list/title/Title.js
+++ src/components/list/title/Title.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Title.css';
 
 const Title = (props) => {
   const { title } = props;
   return <h1 className="List-title">{title}</h1>;
+};
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default Title;
