242d019ca5855b491ed15512d4b3c4abb52accf9
Markus Wind <markuswind@gmail.com>
2018-01-28 15:32:31 +0100

    fixed proptype validation of jokes array

--- src/components/jokes/JokeListComponent.js
+++ src/components/jokes/JokeListComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// import Joke from './../../stores/jokes/models/Joke';
+import Joke from './../../stores/jokes/models/Joke';
 
 import ButtonComponent from './../general/button/ButtonComponent';
 import JokeListTitleComponent from './JokeListTitleComponent';
@@ -12,16 +12,14 @@ import './JokeListComponent.css';
 const JokeListComponent = (props) => {
   const { onRowClick } = props;
 
-  const jokeItems = props.jokes.map(joke => {
-    return (
-      <JokeListItemComponent
-        key={joke.id}
-        joke={joke}
-        onClick={() => { onRowClick(joke); }}
-        buttonTitle={props.rowButtonTitle}
-      />
-    );
-  });
+  const jokeListItemComponents = props.jokes.map(joke => (
+    <JokeListItemComponent
+      key={joke.id}
+      joke={joke}
+      onClick={() => { onRowClick(joke); }}
+      buttonTitle={props.rowButtonTitle}
+    />
+  ));
 
   return (
     <div className="jokeListWrapper">
@@ -30,7 +28,7 @@ const JokeListComponent = (props) => {
         onClick={() => { props.onButtonClick(); }}
         title={props.buttonTitle}
       />
-      <ul className="jokeList">{jokeItems}</ul>
+      <ul className="jokeList">{jokeListItemComponents}</ul>
     </div>
   );
 };
@@ -39,7 +37,7 @@ JokeListComponent.propTypes = {
   title: PropTypes.string.isRequired,
   buttonTitle: PropTypes.string.isRequired,
   rowButtonTitle: PropTypes.string.isRequired,
-  jokes: PropTypes.array, // PropTypes.arrayOf(PropTypes.instanceof(Joke)),
+  jokes: PropTypes.arrayOf(PropTypes.instanceOf(Joke)),
   onRowClick: PropTypes.func.isRequired,
   onButtonClick: PropTypes.func.isRequired,
 };
